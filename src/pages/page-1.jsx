9eef142690d238b4b2d403394b66dbcbe8ebab1c
Seb <sebastien.philippe5@gmail.com>
2020-06-16 20:32:35 +0200

    mise à jour SEO

---
+++ src/pages/page-1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {link} from 'gatsby'
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const page1 = () => (
+    <Layout>
+        <SEO title="Page1" />
+        <h1>Bienvenue sur cette page 2</h1>
+    </Layout>
+)
+
+export default page1
