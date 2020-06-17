3446517b149e6123cf7421dac1c8d29b96c2b759
Seb <sebastien.philippe5@gmail.com>
2020-06-17 14:05:43 +0200

    Mise à jour

--- src/pages/page-1.jsx
+++ src/pages/page-1.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import {link} from 'gatsby'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const page1 = () => (
